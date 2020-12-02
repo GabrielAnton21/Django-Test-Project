cd204749844a46f568e798ea77b9664ecf4b8917
GabrielAnton21 <44251263+GabrielAnton21@users.noreply.github.com>
2020-12-01 20:11:25 -0500

    worked on complete

--- ToDoList/models.py
+++ ToDoList/models.py
@@ -4,6 +4,6 @@ from django.db import models
 # Create your models here.
 
 class Tasks(models.Model):
-    task = models.CharField(max_length=200)
+    task = models.CharField(max_length=200, default="Add Task")
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
